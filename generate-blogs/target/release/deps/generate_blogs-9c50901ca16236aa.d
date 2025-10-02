11ea3b5e384fc75f8b7e59a4b6b77d1f5e0d6623
rookieCookies <rookieCookies@users.noreply.github.com>
2025-10-02 18:29:28 +0000

    deploy: 1aacd38377480203b96ca40c659ba6875271a0b6

---
+++ generate-blogs/target/release/deps/generate_blogs-9c50901ca16236aa.d
@@ -0,0 +1,5 @@
+/home/runner/work/website/website/generate-blogs/target/release/deps/generate_blogs-9c50901ca16236aa.d: src/main.rs
+
+/home/runner/work/website/website/generate-blogs/target/release/deps/generate_blogs-9c50901ca16236aa: src/main.rs
+
+src/main.rs:
---
+++ generate-blogs/target/release/generate-blogs.d
@@ -0,0 +1,1 @@
+/home/runner/work/website/website/generate-blogs/target/release/generate-blogs: /home/runner/work/website/website/generate-blogs/src/main.rs
