80fc1f39f5394430abdfce59a799dfb14d9c700b
*daymare* <87831835+rookieCookies@users.noreply.github.com>
2025-10-02 19:22:58 +0100

    stuff

---
+++ generate-blogs/target/debug/deps/generate_blogs-33226902f2203002.d
@@ -0,0 +1,5 @@
+/Users/macbook/Desktop/projects/actual projects/website/generate-blogs/target/debug/deps/generate_blogs-33226902f2203002.d: src/main.rs
+
+/Users/macbook/Desktop/projects/actual projects/website/generate-blogs/target/debug/deps/libgenerate_blogs-33226902f2203002.rmeta: src/main.rs
+
+src/main.rs:
---
+++ generate-blogs/target/debug/deps/generate_blogs-e233df77da7c721f.d
@@ -0,0 +1,5 @@
+/Users/macbook/Desktop/projects/actual projects/website/generate-blogs/target/debug/deps/generate_blogs-e233df77da7c721f.d: src/main.rs
+
+/Users/macbook/Desktop/projects/actual projects/website/generate-blogs/target/debug/deps/libgenerate_blogs-e233df77da7c721f.rmeta: src/main.rs
+
+src/main.rs:
